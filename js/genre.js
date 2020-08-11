73e631e81d6e350c7f081b4acf9b1dc71b364f2a
AncutaBiro <ancuta.biro@yahoo.com>
2020-08-11 23:20:30 +0300

    getMovies, working on category and images getMoviesByCategory, working on dynamic search

---
+++ js/genre.js
@@ -0,0 +1,50 @@
+window.Category = {
+
+    API_URL: "http://localhost:8082",
+
+    getMoviesByCategory: function () {
+
+        let genre = "horror";
+
+        $.ajax({
+            method: "GET",
+            url: Category.API_URL + "/categories/" + genre
+        }).done(function (response) {
+            console.log(response);
+            Category.displayMoviesFromCategory(response.movies);
+        })
+    },
+
+    getMoviesinCategoryHtml: function (movie) {
+        return `
+<div class="col-md-2 w3l-movie-gride-agile">
+<a href="#" class="hvr-shutter-out-horizontal"><img src="images/m7.jpg" title="album-name" alt=" " />
+<div class="w3l-action-icon"><i class="fa fa-play-circle" aria-hidden="true"></i></div>
+</a>
+<div class="mid-1">
+<div class="w3l-movie-text">
+<h6><a href="#">${movie.title}</a></h6>
+</div>
+<div class="mid-2">
+<p> Genre </p>
+<p>${movie.rate}</p>
+
+<div class="clearfix"></div>
+</div>
+</div>
+</div>
+        `
+    },
+
+    displayMoviesFromCategory: function (movies) {
+        let moviesHtml = '';
+
+        movies.forEach(movie => moviesHtml += Category.getMoviesinCategoryHtml(movie));
+
+        $('.browse-inner').html(moviesHtml);
+        $('.browse-inner-come-agile-w3').html(moviesHtml);
+    },
+
+};
+
+Category.getMoviesByCategory();
---
+++ js/movieListSetRating.js
@@ -0,0 +1,54 @@
+window.Movie = {
+
+    API_URL: "http://localhost:8082",
+
+    getMovies: function () {
+        $.ajax({
+            method: "GET",
+            url: Movie.API_URL + "/movies"
+        }).done(function (response) {
+            console.log(response);
+            Movie.displayMovies(response.content);
+        })
+    },
+
+    getMovieHtml: function (movie) {
+        return `
+                                    </div>
+									<table id="table-breakpoint">
+										<thead>
+										  <tr>
+											<th>No.</th>
+											<th>Movie Name</th>
+											<th>Logline</th>
+											<th>Genre</th>
+											<th>Rating</th>
+										  </tr>
+										</thead>
+										<tbody>
+										  <tr>
+											<td>${movie.id}</td>
+											<td class="w3-list-img"><a href="#"><img src="images/n1.jpg" alt="" /> <span>${movie.title}</span></a></td>
+											<td>${movie.description}</td>
+											<td class="w3-list-info"><a href="#">Comedy, Drama</a></td>
+											<td>${movie.rate}</td>
+										  </tr>
+        `
+    },
+
+    displayMovies: function (movies) {
+        let moviesHtml = '';
+
+        movies.forEach(movie => moviesHtml += Movie.getMovieHtml(movie));
+
+        $('#table-breakpoint').html(moviesHtml);
+
+    },
+
+
+
+
+
+};
+
+Movie.getMovies();
