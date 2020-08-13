2a410a9b141995e206fa92c795a71f9c81b2cc81
AncutaBiro <ancuta.biro@yahoo.com>
2020-08-13 21:15:25 +0300

    working on rate movie in list

--- js/movieListSetRating.js
+++ js/movieListSetRating.js
@@ -23,15 +23,24 @@ window.Movie = {
 											<th>Logline</th>
 											<th>Genre</th>
 											<th>Rating</th>
+											<th></th>				
 										  </tr>
 										</thead>
+										
 										<tbody>
 										  <tr>
 											<td>${movie.id}</td>
 											<td class="w3-list-img"><a href="#"> <img src= "${Movie.API_URL}/images/${movie.poster}" alt="" /> <span>${movie.title}</span></a></td>
 											<td>${movie.description}</td>
 											<td class="w3-list-info"><a href="#">${movie.categories.join(" ")}</a></td>
-											<td>${movie.rate}</td>
+			
+											<td>${movie.rate}
+											<input type="number" step=0.1 min=0 max=10 id="rate-field" placeholder="0-10"> </td>
+										
+                                            <td> 
+                                            <a href="#" class="edit" data-id=${movie.id}>
+                                            <input type="submit" value="Save"> </td>
+											
 										  </tr>
         `
     },
@@ -45,6 +54,32 @@ window.Movie = {
 
     },
 
-};
+    setRateInMovie: function (id, rate) {
+        const requestBody = {
+            rate: rate
+        };
+        $.ajax({
+            url: Movie.API_URL + '?id=' + id + rate,
+            method: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            Movie.getMovies();
+        });
+    },
+
+    bindEvents: function () {
+        $('#table-breakpoint').delegate('.edit', 'submit', function (event) {
+            event.preventDefault();
+            let id = $(this).data('id');
+            let rate = $(this).data('rate');
+            Movie.setRateInMovie(id, rate);
+        });
+
+
+    },
+
+}
 
 Movie.getMovies();
+Movie.bindEvents();
