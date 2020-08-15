51e7fdd52fba211c5583f64d89b58af9069c84ea
AncutaBiro <ancuta.biro@yahoo.com>
2020-08-15 14:22:34 +0300

    implemented setRate with 2 parameters(id, rate) and changed the bindEvents

--- js/movieListSetRating.js
+++ js/movieListSetRating.js
@@ -14,13 +14,13 @@ window.Movie = {
 
     getMovieHtml: function (movie) {
         return `
-										  <tr>
-											<td> ${movie.id}</td>
-											<td class="w3-list-img"><a href="#"> <img src= "${Movie.API_URL}/images/${movie.poster}" alt="" /> 
-											<span> ${movie.title}</span></a>
-											</td>
-											<td> ${movie.description}</td>
-											<td class="w3-list-info"><a href="#">${movie.categories.join(" ")}</a></td>
+				<tr>
+				<td> ${movie.id} </td>
+				<td class="w3-list-img"><a href="#"> <img src= "${Movie.API_URL}/images/${movie.poster}" alt="" /> 
+				<span> ${movie.title}</span></a>
+				</td>
+				<td> ${movie.description}</td>
+				<td class="w3-list-info"><a href="#"> ${movie.categories.join(" ")}</a></td>
 			
 											<td>${movie.rate}
 											<input id=${movie.id + '_input'} type="number" step=0.1 min=0 max=10 id="rate-field" placeholder="0-10"> </td>
@@ -41,15 +41,9 @@ window.Movie = {
 
     },
 
-    setRateInMovie: function (id, movie, rateValue) {
-        let titleValue =  movie.title;
-        let descriptionValue = movie.description;
-        let posterValue = movie.poster;
+    setRateInMovie: function (id, rateValue) {
 
         let requestBody = {
-            title: titleValue,
-            description: descriptionValue,
-            poster: posterValue,
             rate: rateValue
         };
         $.ajax({
@@ -67,16 +61,10 @@ window.Movie = {
             event.preventDefault();
 
             let id = $(this).data('id');
+
             let rate = $('#' + id + '_input').val();
 
-            const movie= {
-                title: "test",
-                description: "test",
-                poster: "t",
-            }
-
-            console.log('#' + id + '_input')
-            Movie.setRateInMovie(id, movie, rate);
+            Movie.setRateInMovie(id, rate);
         });
 
 
